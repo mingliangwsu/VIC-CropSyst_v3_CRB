cb8b0ea2a4c12943116e4a2f0f4cf7e78d2de9fb
Mingliang Liu <mingliang.liu@wsu.edu>
2017-01-13 11:10:37 -0800

    add parameters

---
+++ Simulation/Database/Crop/create_links.sh
@@ -0,0 +1,5 @@
+perl -p -e 's/\r$//' < /media/RAID/Data/Kiran/VIC/Code/Vic_crop/crop/scripts/link_name_code_CS_crop.sh > link_name_code_CS_crop.sh
+perl -p -e 's/\r$//' < /media/RAID/Data/Kiran/VIC/Code/Vic_crop/crop/scripts/link_name_WSDA_CS_crop.sh > link_name_WSDA_CS_crop.sh
+perl -p -e 's/\r$//' < /media/RAID/Data/Kiran/VIC/Code/Vic_crop/crop/scripts/WSDA_code_calibrations.sh > WSDA_code_calibrations.sh
+chmod uga+x *.sh
+
