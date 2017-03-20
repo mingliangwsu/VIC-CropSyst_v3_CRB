9154bcd51be604a47d980181de687c48de2985ba
Mingliang Liu <mingliang.liu@wsu.edu>
2017-03-20 11:48:21 -0700

    added scripts

--- Simulation/Database/Crop/Code/generate_symlink_CS_Suite_default.sh
+++ Simulation/Database/Crop/Code/generate_symlink_CS_Suite_default.sh
@@ -1,34 +1,24 @@
 cd ~/Projects/BioEarth/VIC-CropSyst/Simulation/Database/Crop/Code
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Asterids/Asterales/Asteraceae/Cynareae/Carthamus/tinctorius/"safflower (Carthamus tinctorius) .crp" 1518.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Asterids/Asterales/Asteraceae/Helianthoideae/Heliantheae/Helianthus/annuus/"sunflower (Helianthus annuus).crp" 1531.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Asterids/Asterales/Asteraceae/Helianthoideae/Heliantheae/Helianthus/annuus/"sunflower (Helianthus annuus).crp" 1906.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Asterids/Ericales/Theaceae/Camellia/sinensis/"tea (Camellia sinensis).crp" 804.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Asterids/Solanales/Solanaceae/Solanoideae/Solaneae/Solanum/lycopersicum/"tomato (Solanum lycopersicum).CRP" 1834.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Caryophyllales/Amaranthaceae/Betoideae/Beta/vulgaris/"sugarbeet (Beta vulgaris).crp" 1832.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Brassicales/Brassicaceae/Brassica/napus/"rape (Brassica napus).crp" 2205.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Faboideae/Aeschynomeneae/Arachis/hypogaea/"peanut (Arachis hypogaea).crp" 1840.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Faboideae/Glycine/max/"soybean (Glycine max).crp" 1907.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Faboideae/Phaseoleae/Paseolinae/Phaseolus/vulgaris/"Dry beans (Phaseolus vulgaris).crp" 1803.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Faboideae/Vicieae/Lens/culinaris/"lentil (Lens culinaris).crp" 1819.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Faboideae/Vicieae/Pisum/sativum/"pea (Pisum sativum).crp" 1824.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Fabales/Fabaceae/Medicago/sativa/"alfalfa (Medicago sativa).crp" 701.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Eudicots/Rosids/Malvales/Malvoideae/Gossypieae/Gossypium/hirsutum/"cotton (Gossypium hirsutum).crp" 2201.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Asparagales/Amaryllidaceae/Allioideae/Allium/sativum/"garlic (Allium sativum).crp" 1817.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Avena/sativa/"oats (Avena sativa).crp" 206.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Festuca/"fescue (Festuca).crp" 1526.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Oryza/sativa/"asian rice (Oryza sativa).crp" 2202.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Panicoideae/Andropogoneae/Saccharum/"sugarcane (Saccharum).crp" 2206.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Panicoideae/Andropogoneae/Sorghum/bicolor/"sorghum (Sorghum bicolor).crp" 720.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Panicoideae/Andropogoneae/Zea/mays/"maize (Zea mays).crp" 1814.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Panicoideae/Pennisetum/glaucum/"millet - pearl (Pennisetum glaucum).crp" 2203.crp
-ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Pooideae/Lolium/perenne/"ryegrass - perennial (Lolium perenne).crp" 207.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Pooideae/Triticeae/Hordeum/"barley - winter (Hordeum vulgare).crp" 201.crp
 ln -s ../name/Phylogenetic/Plantae/Angiosperms/Monocots/Commelinids/Poales/Poaceae/Pooideae/Triticeae/Hordeum/"barley - spring (Hordeum vulgare).crp" 702.crp
 
 
 
 ln -s 206.crp 705.crp
-ln -s 207.crp 1517.crp
 ln -s 701.crp 703.crp
 ln -s 701.crp 1501.crp
 ln -s 1803.crp 1522.crp
---
+++ Simulation/Database/Crop/Code/generate_symlink_Stewart.sh
@@ -0,0 +1,16 @@
+ln -s ../name/From_Stewart/"Broccoli OFoot Generic.crp" 1807.crp
+ln -s ../name/From_Stewart/"Cabbage OFoot Generic.crp" 1809.crp
+ln -s ../name/From_Stewart/"Carrot OFoot Generic.crp" 1810.crp
+ln -s ../name/From_Stewart/"Carrot Seed OFoot Generic.crp" 1507.crp
+ln -s ../name/From_Stewart/"Cauliflower OFoot Generic.crp" 1811.crp
+ln -s ../name/From_Stewart/"Flax OFoot Generic.crp" 1903.crp
+ln -s ../name/From_Stewart/"Garlic OFoot Generic.crp" 1817.crp
+ln -s ../name/From_Stewart/"Lettuce Leaf OFoot Generic.crp" 1820.crp
+ln -s ../name/From_Stewart/"Onion Bulb OFoot Generic.crp" 1822.crp
+ln -s ../name/From_Stewart/"Pepper Sweet OFoot Generic.crp" 1826.crp
+ln -s ../name/From_Stewart/"Radish OFoot Generic.crp" 1839.crp
+ln -s ../name/From_Stewart/"Rye cover OFoot Generic.crp" 207.crp
+ln -s ../name/From_Stewart/"Spring Mustard seed OFoot Generic.crp" 1523.crp
+ln -s ../name/From_Stewart/"Squash Summer OFoot Generic.crp" 1831.crp
+ln -s ../name/From_Stewart/"Sudangrass OFoot Generic.crp" 721.crp
+ln -s ../name/From_Stewart/"Triticale OFoot Generic.crp" 209.crp
