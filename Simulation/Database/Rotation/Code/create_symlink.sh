bac22dcc64f507c020a44655fcb013ebb6ca61ae
Mingliang Liu <mingliang.liu@wsu.edu>
2017-03-28 11:13:06 -0700

    create create_symlink.sh

---
+++ Simulation/Database/Rotation/Code/create_symlink.sh
@@ -0,0 +1,5 @@
+#ln -s ../Name/CT_SW-SP-WW_high.rot 3001.CS_rotation
+#ln -s ../Name/CT_SF-WW-SW_intermediate.rot 3002.CS_rotation
+#ln -s ../Name/CT_SF-WW_low.rot 3003.CS_rotation
+ln -s ../Name/CT_P-WW-C_irrigated.rot 4001.CS_rotation
+
